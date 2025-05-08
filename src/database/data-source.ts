6e2f214b301125cebf90db63f0ebfe37573d7491
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-08 13:01:33 -0300

    Novos arquivos env e instalação do postgresql

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,18 +1,36 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import {CreateUsersTable1746650050453} from './migrations/1746650050453-CreateUsersTable'
-import { Company } from "../app/entities/company"
+import { DataSource } from "typeorm";
+import { Company } from "../app/entities/company";
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "mysql",
-    database: "bd_cred",
-    synchronize: true,
-    logging: false,
-    entities: [Company],
-    migrations: [CreateUsersTable1746650050453],
-    subscribers: [],
-})
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [Company],
+  synchronize: false, // use migrations
+  logging: true,
+});
+
+
+
+// import "reflect-metadata"
+// import { DataSource } from "typeorm"
+// import {CreateUsersTable1746650050453} from './migrations/1746650050453-CreateUsersTable'
+// import { Company } from "../app/entities/company"
+
+// export const AppDataSource = new DataSource({
+//     type: "mysql",
+//     host: "localhost",
+//     port: 3306,
+//     username: "root",
+//     password: "mysql",
+//     database: "bd_cred",
+//     synchronize: true,
+//     logging: false,
+//     entities: [Company],
+//     migrations: [CreateUsersTable1746650050453],
+//     subscribers: [],
+// })
+
--- src/database/migrations/1746650050453-CreateUsersTable.ts
+++ src/database/migrations/1746650050453-CreateUsersTable.ts
@@ -1,79 +1,74 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsersTable1746650050453 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.createTable(new Table({
-            name: "Company",
-            columns: [
-                {
-                    name: "id",
-                    type: "int",
-                    isPrimary: true,
-                    isGenerated: true,
-                    generationStrategy: "increment",
-                },
-                {
-                    name: "name_company",
-                    type: "varchar",
-                    length: "255",
-                    isNullable: false,
-                },
-                {
-                    name: "cnpj_company",
-                    type: "varchar",
-                    length: "20",
-                    isUnique: true,
-                    isNullable: false,
-                },
-                {
-                    name: "address",
-                    type: "varchar",
-                    length: "255",
-                    isNullable: false,
-                },
-                {
-                    name: "telephone",
-                    type: "varchar",
-                    length: "20",
-                    isNullable: true,
-                },
-                {
-                    name: "email",
-                    type: "varchar",
-                    length: "255",
-                    isNullable: true,
-                },
-                {
-                    name: "foundation_date",
-                    type: "timestamp",
-                    isNullable: true,
-                },
-                {
-                    name: "createdAt",
-                    type: "timestamp",
-                    default: "now()",
-                    isNullable: false,
-                },
-                {
-                    name: "updatedAt",
-                    type: "timestamp",
-                    default: "now()",
-                    isNullable: false,
-                }
-            ],
-            indices: [
-                {
-                    name: "IDX_COMPANY_CNPJ",
-                    columnNames: ["cnpj_company"],
-                    isUnique: true,
-                },
-            ]
-        }));
-    }
+    await queryRunner.createTable(
+      new Table({
+        name: "Company",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "name_company",
+            type: "varchar",
+            length: "255",
+            isNullable: false,
+          },
+          {
+            name: "cnpj_company",
+            type: "varchar",
+            length: "20",
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: "address",
+            type: "varchar",
+            length: "255",
+            isNullable: false,
+          },
+          {
+            name: "telephone",
+            type: "varchar",
+            length: "20",
+            isNullable: true,
+          },
+          {
+            name: "email",
+            type: "varchar",
+            length: "255",
+            isNullable: true,
+          },
+          {
+            name: "foundation_date",
+            type: "timestamp",
+            isNullable: true,
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()",
+            isNullable: false,
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "now()",
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
 
-    // Tudo que eu fizer acima pode ser desfeito abaixo
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("Company");
+  }
 }
