1dc812dd0dfec2e56e8aab4f7fdbe8aaee6e2411
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-08 14:05:22 -0300

    Env exemplo e nova modelagem do banco de dados

--- src/app/entities/company.ts
+++ src/app/entities/company.ts
@@ -1,23 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity('company')
 export class Company {
   @PrimaryGeneratedColumn('increment')
   id: number;
 
-  @Column({ length: 255 })
+  @Column({ type: 'varchar', length: 255 })
   name_company: string;
 
-  @Column({ length: 20, unique: true })
+  @Column({ type: 'varchar', length: 20, unique: true })
   cnpj_company: string;
 
-  @Column({ length: 255 })
+  @Column({ type: 'varchar', length: 255 })
   address: string;
 
-  @Column({ length: 20, nullable: true })
+  @Column({ type: 'varchar', length: 20, nullable: true })
   telephone: string;
 
-  @Column({ length: 255, nullable: true })
+  @Column({ type: 'varchar', length: 255, nullable: true })
   email: string;
 
   @Column({ type: 'timestamp', nullable: true })
