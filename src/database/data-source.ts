994d7d0f019712701b87d8ed9a5c002822a469a8
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-21 12:15:01 -0300

    Conectar por url ao banco de dados

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,11 +6,12 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
+  // host: process.env.DB_HOST,
+  // port: Number(process.env.DB_PORT) || 5432,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
   ssl: {
     rejectUnauthorized: false,
   },
