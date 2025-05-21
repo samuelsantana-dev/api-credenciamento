a54b1036e812a931ebb32718f08e513861e2cc26
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-21 12:31:20 -0300

    Usar somente a url

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,11 +6,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: [Company],
   migrations: ["src/database/migrations/*.ts"], 
   migrationsTableName: "migrations",
