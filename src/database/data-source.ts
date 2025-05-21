04bcbaa8b1f0949da2bf7e28b48af58242606f6d
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-21 13:01:47 -0300

    Novos ajustes em configuarações

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,5 +1,4 @@
 import { DataSource } from "typeorm";
-import { Company } from "../app/entities/company";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -7,7 +6,7 @@ dotenv.config();
 export const AppDataSource = new DataSource({
   type: "mysql",
   url: process.env.DATABASE_URL,
-  entities: [Company],
+  entities: ["build/entities/*.js"],
   migrations: ["src/database/migrations/*.ts"], 
   migrationsTableName: "migrations",
   synchronize: true,
