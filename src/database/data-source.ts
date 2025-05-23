5f1cbb9dc4daa12554dedfac764600108c27e93d
samuels-space <samuel@00px.com.br>
2025-05-23 15:45:01 -0300

    Novo data source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,14 +1,17 @@
+import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mysql",
   url: process.env.DATABASE_URL,
-  entities: ["build/entities/*.js"],
-  migrations: ["src/database/migrations/*.ts"], 
+  entities: [path.join(__dirname, "../entities/*.js")],
+  migrations: [path.join(__dirname, "../migrations/*.ts")],
   migrationsTableName: "migrations",
-  synchronize: true,
+  synchronize: false,
   logging: true,
+  migrationsRun: true,
 });
