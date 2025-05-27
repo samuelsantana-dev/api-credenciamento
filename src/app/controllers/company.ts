858e55b68d63a1b160076aee3c31dc054fa2fb13
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-05-27 19:52:14 -0300

    Removido comentarios de server e adicionado try catch

--- src/app/controllers/company.ts
+++ src/app/controllers/company.ts
@@ -5,8 +5,13 @@ import  companyRepository  from "../repositories/company";
 const companyController = Router();
 
 companyController.get('/companies', async (_req: Request, res: Response): Promise<void> => {
-    const companies = await companyRepository.getUsers();
-    res.status(200).json(companies);
-}); 
+    try {
+        const companies = await companyRepository.getUsers();
+        res.status(200).json(companies);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao buscar empresas', details: error });
+    }
+});
 
 export default companyController
--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,9 @@ import routers from './app/routes/company';
 
 const app = express();
 
-// Configurações básicas
 app.use(cors());
 app.use(express.json());
 
-// Rota de health check obrigatória para o Railway
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'online' });
 });
@@ -27,10 +25,9 @@ AppDataSource.initialize()
       console.log(`Server running on port ${PORT}`);
     });
 
-    // Timeout para evitar conexões pendentes
     server.setTimeout(5000);
   })
   .catch(error => {
     console.error("Database connection error:", error);
-    process.exit(1); // Encerra o processo se o banco falhar
+    process.exit(1);
   });
